0466a6615e03b4abe1b064c4d69610f70484da91
Ishalini <87859854+IT20134280@users.noreply.github.com>
2022-05-06 12:16:39 +0530

    Update App.js

--- shopping_cart/src/App.js
+++ shopping_cart/src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
+ 
 import Header from "./components/Header";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <div>
-        <Route path='/' exact>
-          <Home/>
-        </Route>
-        <Route path='/cart' exact>
-          <Cart/>
-        </Route>
+        
+           <Route path="/">
+             <h1>Hello</h1>
+           </Route>
+        
+       
       </div>
     </BrowserRouter>
   );
