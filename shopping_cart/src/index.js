4239dd103877be315c40ea5bdacec01cde25414e
Ishalini <87859854+IT20134280@users.noreply.github.com>
2022-05-06 17:31:07 +0530

    f

--- shopping_cart/src/index.js
+++ shopping_cart/src/index.js
@@ -7,9 +7,9 @@ import CartState from "./context/mycart/CartState";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CartState>
+    <CartContext>
     <App />
-    </CartState>
+    </CartContext>
      
   </React.StrictMode>
 );
